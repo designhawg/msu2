2a5f3e3295ba63bd286545a46cbf1e37e3079cdf
Chris Courtney <designhawg@newCheat.local>
2017-01-28 16:04:14 -0500

    update data

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
-var ordinalScale = d3.scaleOrdinal()
-  .domain(['cold', 'cool', 'ok', 'nice', 'warm'])
-  .range(['blue','lightblue','white', 'yellow', 'green']);
+d3.json("data/data.json", function(data){
 
-console.log(ordinalScale('cool'));
-console.log(ordinalScale('warm'));
-console.log(ordinalScale('ok'));
+  var min = d3.min(data, function(d){
+    return d.age;
+  });
+
+  var max = d3.max(data, function(d){
+    return d.age;
+  });
+
+  var ages = d3.set(data, function(d){
+    return d.age;
+  })
+
+  console.log(ages.values());
+
+
+})
+
+
+
+
+
+
+
